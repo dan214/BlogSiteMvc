4bba8967e73901eda1af2deaa6905e12f1b597e6
dan_the_great <danielmiji@gmail.com>
2017-03-25 19:33:31 +0300

    Commiting Articulate package

---
+++ BlogSiteMvc/Controllers/CommentSurfaceController.cs
@@ -0,0 +1,31 @@
+﻿using BlogSiteMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace BlogSiteMvc.Controllers
+{
+    public class CommentSurfaceController : SurfaceController
+    {
+        // GET: CommentSurface
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult HandleFormPost(CommentsModel model)
+        {
+            var newComment = Services.ContentService.CreateContent(model.Email, CurrentPage.Parent.Id, "comments");
+            newComment.SetValue("commentName", model.CommentName);
+            newComment.SetValue("email", model.Email);
+            newComment.SetValue("phone", model.Phone);
+            newComment.SetValue("message", model.Message);
+
+            Services.ContentService.SaveAndPublishWithStatus(newComment);
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
---
+++ BlogSiteMvc/Models/CommentsModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlogSiteMvc.Models
+{
+    public class CommentsModel
+    {
+        public string CommentName { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+        public string Message { get; set; }
+        public int BlogItemId { get; set; }
+    }
+}
